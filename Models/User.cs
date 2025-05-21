efe91df1cba99822d16a4a3528d55f5409131fbd
Ziad Atta <ziadatta2233@gmail.com>
2025-05-21 17:03:15 +0300

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Examination_System.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string Email { get; set; }
+        public string? PasswordHash { get; set; }
+        public UserRole? Role { get; set; } 
+    }
+
+    public enum UserRole
+    {
+        Instructor,
+        Student
+    }
+}
